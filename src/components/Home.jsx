caee63ae87934e58fd1c987db93f3ee1ffb618bd
Reshav <thapareshav50@gmail.com>
2023-05-25 17:05:53 +0545

    Build: icon package is installed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import './Home.css'
-import {GoSearch} from 'react-icons/go'
+import Post from "./Post.jsx";
+import "./Home.css";
+
+import { GoSearch } from "react-icons/go";
 
 const Home = () => {
   const [searchQuery, setSearch] = useState("");
@@ -18,17 +20,21 @@ const Home = () => {
   };
 
   return (
-    <div className = "search">
+    <div className="search">
       <input
-        className = "search-input"
+        className="search-input"
         type="text"
         value={searchQuery}
-        onChange ={handleSearch}
+        onChange={handleSearch}
         placeholder="Search..."
       />
-      <button className = "search-btn" onClick={handleSearch}>
+      <button className="search-btn" onClick={handleSearch}>
         <GoSearch />
       </button>
+
+      <div className="create_post">
+        <Post />
+      </div>
 
       {/* Render the filtered results */}
       <ul>
