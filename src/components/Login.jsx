6e90c40266b69ce50985fb47f3ef865bf0958df8
Aarjan <aarjan.pat@gmail.com>
2023-05-25 18:13:52 +0545

    added login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import "./Login.css"
+import {useNavigate} from "react-router-dom"
+import logo from './LoginImage.png'
 
 function Login() {
+  const navigate = useNavigate()
   return (
-    <div>
-      <h1></h1>
+    <div className='Login'>
+      <div className='login--body'>
+        <div className='form'>
+          <form>
+            <h1>User Login</h1>
+            <input type="email" name='email' placeholder='Email'></input>
+            <input type="password" name="password" placeholder='Password'></input>
+            <button type='submit' onClick={()=>navigate('home')} >LOGIN</button>
+            <h4>Not a member?<span>  <u>Sign up</u></span></h4>
+          </form>
+        </div>
+
+        <div className='media--logo'>
+          <img src={logo} alt="Logo"/>
+        </div>
+      </div>
     </div>
   )
 }
