17c8aa8374a2ddba27a1295a5b3889a215454ebc
Reshav <thapareshav50@gmail.com>
2023-06-05 19:05:59 +0545

    json server update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "../src/components/Home.jsx";
 import Login from "../src/components/Login:signup/Login.jsx";
 import Register from "../src/components/Login:signup/Register";
 import UserProfile from '../src/components/Users/UserProfile'
+import UserList from "./components/Users/userList";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/home" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/users" element={<UserProfile />} />
+          <Route path = "/userlist" element={<UserList />} />
         </Routes>
       </div>
     </Router>
---
+++ client/src/components/Users/UserList.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
+function UserList() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+      }
+    );
+  }, []);
+
+  return (
+    <div>
+        {(typeof data.users === 'undefined')?(
+          <p>Loading ...</p>
+        ): (
+          data.users.map((user, i) => (
+            <p key = {i}>{user}</p>
+          ))
+        )}
+    </div>
+  );
+}
+
+export default UserList;
